f980d15cfd5c093c19fdc56d7ced0a12ffdbc742
Josue Valenzuela <val171001@uvg.edu.gt>
2022-10-23 23:53:46 -0600

    fix func name

--- fancontrol.py
+++ fancontrol.py
@@ -3,7 +3,7 @@ import os
 
 IMPITOOL =  'ipmitool'
 
-def _exec_raw_impi_cmd(payload: str) -> int:
+def _exec_raw_ipmi_cmd(payload: str) -> int:
     cmd = f'{IMPITOOL} raw {payload}'
     return os.system(cmd)
 
@@ -15,13 +15,13 @@ def _fan_speed_command(speedHex: str) -> str:
     return f'0x30 0x30 0x02 0xff {speedHex}'
 
 def enable_fancontrol() -> int:
-    return _exec_raw_impi_cmd(_manual_fancontrol_toggle_command(enabled=True))
+    return _exec_raw_ipmi_cmd(_manual_fancontrol_toggle_command(enabled=True))
 
 def disable_fancontrol() -> int:
-    return _exec_raw_impi_cmd(_manual_fancontrol_toggle_command(enabled=False))
+    return _exec_raw_ipmi_cmd(_manual_fancontrol_toggle_command(enabled=False))
 
 def set_manual_fanspeed(speed: int) -> int:
     if 100 >= speed >= 0:
-        return _exec_raw_impi_cmd(_fan_speed_command(hex(speed)))
+        return _exec_raw_ipmi_cmd(_fan_speed_command(hex(speed)))
     else:
         raise Exception(f'Invalid speed {speed}. Fan speed must be between 0 and 100')
